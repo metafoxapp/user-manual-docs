e6a0c73db268857328c592bb022e3a32b56848df
PhongBN <phongbn@younetgroup.com>
2026-03-11 09:10:25 +0700

    Updates

--- _next/static/chunks/nextra-page-map-.mjs
+++ _next/static/chunks/nextra-page-map-.mjs
@@ -180,7 +180,7 @@ export const pageMap = [{
     name: "dowload-metafox-source",
     route: "/getting-started/dowload-metafox-source",
     frontMatter: {
-      "title": "Download MetaFox Source"
+      "title": "Download MetaFox"
     }
   }, {
     name: "essential-steps-after-installing-metafox",
