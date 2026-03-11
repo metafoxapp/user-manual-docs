97facd34d9022c96fcb8e5b9a53f49a6a5615959
PhongBN <phongbn@younetgroup.com>
2026-03-11 09:12:50 +0700

    update download guide

--- pages/getting-started/_meta.js
+++ pages/getting-started/_meta.js
@@ -1,7 +1,8 @@
 export default {
   "system-requirements": "System Requirements",
-  "dowload-metafox-source": "Dowload MetaFox Source",
+  "dowload-metafox-source": "Download MetaFox",
   "install-metafox": "Install MetaFox",
   "update-metafox": "Update MetaFox",
   "essential-steps-after-installing-metafox": "Essential steps after installing MetaFox",
 }
+  
