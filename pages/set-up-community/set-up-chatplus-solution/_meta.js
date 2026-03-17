8136a6a742cd36068305b5caddc1d0254a94393a
Son Vu <sonvlh@younetgroup.com>
2026-03-17 15:14:44 +0700

    MFOXCORE-9618

--- pages/set-up-community/set-up-chatplus-solution/_meta.js
+++ pages/set-up-community/set-up-chatplus-solution/_meta.js
@@ -2,6 +2,6 @@ export default {
   "chatplus-server-requirements": "ChatPlus Server Requirements",
   "set-up-chatgpt-bot": "Set up ChatGPT Bot",
   "set-up-chatplus-server": "Set up ChatPlus server",
-  "set-up-reverse-proxy-with-nginx": "Set up reverse proxy with NginX",
+  "set-up-reverse-proxy-with-nginx": "Set up reverse proxy with Nginx",
   "update-chatplus-server": "Update ChatPlus server",
 }
