c2799d47562a8b51d032abc1e8614b20caab1810
Son Vu <sonvlh@younetgroup.com>
2026-03-20 16:06:37 +0700

    add user guide for subscription usage

--- pages/set-up-community/set-up-subscription/_meta.js
+++ pages/set-up-community/set-up-subscription/_meta.js
@@ -1,4 +1,5 @@
 export default {
   "create-subscription-packages": "Create Subscription packages",
   "enable-subscription-on-registration": "Enable Subscription on Registration",
+  "how-subscription-works": "How Subscription works",
 }
