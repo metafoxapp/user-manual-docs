4fc77bbb61466c574f1cf3e4802fa80dcb8e4a93
PhongBN <phongbn@younetgroup.com>
2026-03-27 09:58:41 +0700

    add article for configuring ad placement for sponsored items

--- pages/set-up-community/set-up-advertise/_meta.js
+++ pages/set-up-community/set-up-advertise/_meta.js
@@ -1,4 +1,5 @@
 export default {
   "create-advertise-placements": "Create Advertise placements",
   "grant-user-permission-to-create-ads": "Grant user permission to create ads",
+  "configure-ad-placement-for-sponsored-items": "Configure ad placement for sponsored items",
 }
